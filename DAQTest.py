35866baafbaf0b3ab15e995d242e24e7aea9bb9e
Joshua Beard <joshuabeard@Joshuas-MacBook-Pro-2.local>
2025-04-02 18:08:03 -0700

    .

--- DAQTest.py
+++ DAQTest.py
@@ -8,6 +8,7 @@ spi.open(0, 0)
 spi.xfer2([0b00001000])
 
 while True:
-    recieved = spi.xfer2([0b00010010])
-    print(recieved)
+    recieved = spi.xfer2([0b00010010, 0x00, 0x00, 0x00])
+    data = (recieved[1] << 16) | (recieved[2] << 8) | recieved[3]
+    print(data)
     time.sleep(0.1)
