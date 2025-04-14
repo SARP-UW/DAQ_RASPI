a5a7322baf8a6ff34908b9a034a5dc884a1e99d6
Joshua Beard <joshuabeard@Joshuas-MacBook-Pro-2.local>
2025-04-13 20:54:39 -0700

    .

--- DAQTest.py
+++ DAQTest.py
@@ -7,7 +7,7 @@ spi.open(0, 0)
 spi.max_speed_hz = 1000000
 spi.mode = 0x01
 
-# spi.xfer2([0b00001000])
+spi.xfer2([0b00001000])
 
 while True:
     recieved = spi.xfer2([0b00010010, 0x00, 0x00, 0x00])
