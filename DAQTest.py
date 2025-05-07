0da3724e46165f7d54910ea75f473a7619935168
Joshua Beard <joshuabeard@Joshuas-MacBook-Pro-2.local>
2025-05-06 22:08:40 -0700

    .

--- DAQTest.py
+++ DAQTest.py
@@ -13,9 +13,10 @@ spi.mode = 0x01
 GPIO.setmode(GPIO.BCM)
 
 GPIO.setup(17, GPIO.OUT)
-GPIO.setup(27, GPIO.IN)
+GPIO.setup(27, GPIO.OUT)
 
 GPIO.output(17, GPIO.HIGH)
+GPIO.output(27, GPIO.LOW)
 time.sleep(0.2)
 
 
@@ -26,10 +27,13 @@ GPIO.output(17, GPIO.HIGH)
 while True:
 
     time.sleep(0.2)
-
+    GPIO.output(27, GPIO.HIGH)
+    time.sleep(0.05)
     GPIO.output(17, GPIO.LOW)
     recieved = spi.xfer2([0b00010010, 0x00, 0x00, 0x00])
+    time.sleep(0.05)
     GPIO.output(17, GPIO.HIGH)
+    GPIO.output(27, GPIO.LOW)
 
     data = (recieved[1] << 16) | (recieved[2] << 8) | recieved[3]
     if data != 0:
