a8bac2835f5999e00932b142d9ec4049eb4c8e51
Joshua Beard <joshuabeard@Joshuas-MacBook-Pro-2.local>
2025-05-07 19:32:04 -0700

    .

--- DAQTest.py
+++ DAQTest.py
@@ -15,9 +15,10 @@ GPIO.output(CS, GPIO.HIGH)
 time.sleep(0.1)
 
 GPIO.output(CS, GPIO.LOW)
-returned = spi.xfer2([0b00100101, 0b00000001, 0, 0])  # WREG, 2 bytes: INPMUX=1, PGA=0
-GPIO.output(CS, GPIO.HIGH)
 time.sleep(0.1)
+returned = spi.xfer2([0b00100101, 0b00000001, 0xFF, 0xFF])  # WREG, 2 bytes: INPMUX=1, PGA=0
+time.sleep(0.1)
+GPIO.output(CS, GPIO.HIGH)
 
 print(returned)
 
